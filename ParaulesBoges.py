b98092afe61cdc8e7632efb0b7fc23194040404a
roger.domingo.7e7 <roger.domingo.7e7@itb.cat>
2024-02-23 12:09:30 +0100

    hola

---
+++ ParaulesBoges.py
@@ -0,0 +1,8 @@
+"""
+Roger Domingo Meléndez
+23/02/2024
+ASIXc M03 UF2 pt1 R1: Disseny descendent: Dividir en funcions
+Descripció: ParaulesBoges.
+Implementar en Python un programa que demani una frase per teclat i la retorni per pantalla amb les lletres de cada paraula de la frase desordenada, tal com diu l’estudi de la Universitat de Cambridge.
+
+"""
