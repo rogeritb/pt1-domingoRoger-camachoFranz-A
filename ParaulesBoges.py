cde77b74ad1dfd42da39fbf6f26f03e53865bc12
roger.domingo.7e7 <roger.domingo.7e7@itb.cat>
2024-02-23 12:22:06 +0100

    hola

--- ParaulesBoges.py
+++ ParaulesBoges.py
@@ -6,3 +6,25 @@ Descripció: ParaulesBoges.
 Implementar en Python un programa que demani una frase per teclat i la retorni per pantalla amb les lletres de cada paraula de la frase desordenada, tal com diu l’estudi de la Universitat de Cambridge.
 
 """
+import random
+
+def desordenar_frase(frase):
+    paraules = frase.split()
+    frase_desordenada = ""
+
+    for palabra in paraules:
+        lletres = list(palabra)
+        primera_lletra = lletres.pop(0)
+        ultima_lletra = lletres.pop()
+
+        random.shuffle(lletres)
+
+        paraula_desordenada = primera_lletra + ''.join(lletres) + ultima_lletra
+        frase_desordenada += paraula_desordenada + ' '
+
+    return frase_desordenada.rstrip()
+
+
+frase = input("Introdueix una frase: ")
+frase_desordenada = desordenar_frase(frase)
+print("Frase desordenada:", frase_desordenada)
